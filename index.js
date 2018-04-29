9a7921d69e5714fc87b0a6de3cccd4ab32bf7584
Kyle Stevenson <ky_steve@hotmail.com>
2018-04-28 20:27:12 -0400

    test

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const NewsApi = require("newsapi");
-const newsapi = new NewsApi('1f62f144d9584aaeb3fb553f42c989a6');
-
+const newsapi = new NewsApi("1f62f144d9584aaeb3fb553f42c989a6");
 const extractor = require("unfluff");
-
 const axios = require("axios");
+const knex = require("knex")({
+    client: 'sqlite3',
+    connection: { filename: "./mydb.sqlite" }
+})
 
 run();
+
+function createTable(){
+    knex.schema.createTable('articles', function(table){
+        table.dateTime('date');
+        table.string('url');
+        table.string('title');
+        table.string('text');
+        table.string('topics');
+        table.string('keywords');
+        table.string('entities');
+    });
+}
+
+function insert(data){
+    knex.insert(data).into('articles')
+}
 
 async function getTopHeadlines(){
     return await newsapi.v2.topHeadlines({
@@ -14,7 +32,7 @@ async function getTopHeadlines(){
 }
 
 async function getHtml(url){
-    return await axios(url);
+    if(isUrl(url)) return await axios(url);
 }
 
 async function getExtractedContent(html){
@@ -22,18 +40,48 @@ async function getExtractedContent(html){
 }
 
 async function run(){
+    let entries = [];
+
     try {
+        //create sqlite table if it doesnt exist
+        createTable();
+
+        //get top headlines from newsapi
         let headlines = await getTopHeadlines();
-        let htmls = headlines.map(async function(x){
-            return await getHtml(x); 
+        headlines.articles.map(x => {
+            let entry = {}
+            entry.date = x.date;
+            entry.url = x.url;
+            entry.title = x.title;
+            entries.push(entry);
         });
+
+        //get html page from headline urls
+        let htmls = entries.map(async function(x){
+            return await getHtml(x.url); 
+        });
+
+        //extract article content from html pages
+        let texts = htmls.map(async function(x){
+            return await getExtractedContent(x);
+        });
+
+        //TODO: get topics/keywords/entities
+
+        //insert data into sqlite db
+        insert(entries);
+
     } catch (error) {
         console.log(error);
     }
 }
 
-
-
-
-
-//module.exports.getTopHeadlines = getTopHeadlines;     
+/**
+ * @description determines if a given string is in url format or not
+ * @param {String} s string to test for url format
+ * @returns {Boolean} true if string is url format
+ */
+function isUrl(s){
+    var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
+}
